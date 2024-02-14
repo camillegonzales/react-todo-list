8710e0006e81b901244fcf80b315f13abc347bf6
camillegonzales <gonzcami@oregonstate.edu>
2024-02-14 12:33:45 -0800

    added deleteTodo function and event listener

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,15 @@ export default function App() {
         if (todo.id === id) {
           return { ...todo, completed }
         }
-        
+
         return todo
       })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
     })
   }
 
@@ -56,7 +62,7 @@ export default function App() {
             />
             {todo.title}
           </label>
-            <button className="btn btn-danger">Delete</button>
+            <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
         </li>
         })}
       </ul>
