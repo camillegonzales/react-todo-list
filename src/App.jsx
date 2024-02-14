4033a26d8b465e4949c98df424996819cf366490
camillegonzales <gonzcami@oregonstate.edu>
2024-02-14 12:12:06 -0800

    added submit handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
+import { useState } from "react"
 import "./styles.css"
 
 export default function App() {
-  return <form className="new-item-form">
-    <div className="form-row">
-      <label htmlFor="item">New Item</label>
-      <input type="text" id="item"/>
-    </div>
-  </form>
+  const [newItem, setNewItem] = useState("")
+
+  function handleSubmit(e) {
+    e.preventDefault()
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input 
+            value={newItem} 
+            onChange={e => setNewItem(e.target.value)} 
+            type="text" 
+            id="item"
+          />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list"></ul>
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+            <button className="btn btn-danger">Delete</button>
+        </li>
+    </>
+  )
 }
